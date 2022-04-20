71f3c1415742bbe7c4e37dacee585c549f071b79
cam900 <dbtlrchl@naver.com>
2022-04-21 02:18:33 +0900

    Add source and example

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "resource_list.hpp"
+#include <iostream>
+
+int main(int arg_num, char** arg_val)
+{
+	resource_list<int> something;
+    // init
+	std::cout << "list size is " << something.size() << std::endl;
+	resource_id a = something.insert(30);
+	resource_id b = something.insert(72);
+	resource_id c = something.insert(17);
+    // get size
+	std::cout << "list size is " << something.size() << std::endl;
+	for (auto it = something.cbegin(); it != something.cend(); it++)
+	{
+		std::cout << "id " << it->first << ": " << it->second << std::endl;
+	}
+    // remove something and get current status
+	something.erase(b);
+	std::cout << "list size is " << something.size() << std::endl;
+	for (auto it = something.cbegin(); it != something.cend(); it++)
+	{
+		std::cout << "id " << it->first << ": " << it->second << std::endl;
+	}
+	something.erase(a);
+    // add another thing
+	resource_id d = something.emplace(30);
+	resource_id e = something.emplace(11);
+	resource_id f = something.emplace(21);
+	std::cout << "list size is " << something.size() << std::endl;
+	for (auto it = something.cbegin(); it != something.cend(); it++)
+	{
+		std::cout << "id " << it->first << ": " << it->second << std::endl;
+	}
+	return 0;
+}
---
+++ resource_list.hpp
@@ -0,0 +1,193 @@
+/*
+	Resource list by cam900
+
+	usage:
+
+	top of source:
+	#include "where/to/resource_list.hpp"
+
+	construct:
+	resource_list<class> something;
+
+	destruct:
+	~something;
+
+	insert something to list:
+	resource_id a = something.insert(anything);
+	or
+	resource_id a = something.emplace(anything);
+
+	erase something from list:
+	something.erase(a);
+
+	get data from list:
+	class b = something.get(a);
+
+	check error string:
+	get_error_string();
+*/
+
+#ifndef _RESOURCE_LIST
+#define _RESOURCE_LIST
+
+#pragma once
+
+#include <functional>
+#include <unordered_map>
+#include <queue>
+#include <string>
+
+typedef std::size_t resource_id;
+
+template<class _Ty>
+class resource_list
+{
+public:
+	// constructor
+	resource_list()
+		: error_string("")
+		, stored_list()
+		, deleted_list()
+	{}
+
+	// destructor
+	~resource_list()
+	{
+		if (!stored_list.empty())
+		{
+			stored_list.clear();
+		}
+		while (!deleted_list.empty())
+		{
+			deleted_list.pop();
+		}
+	}
+
+	// add resource to list
+	resource_id insert(_Ty source)
+	{
+		error_string = "No error.";
+		resource_id id = stored_list.size();
+		if (!deleted_list.empty())
+		{
+			id = deleted_list.top();
+			deleted_list.pop();
+		}
+		stored_list.insert(std::make_pair(id, source));
+		return id;
+	}
+
+	resource_id emplace(_Ty source)
+	{
+		error_string = "No error.";
+		resource_id id = stored_list.size();
+		if (!deleted_list.empty())
+		{
+			id = deleted_list.top();
+			deleted_list.pop();
+		}
+		stored_list.emplace(id, source);
+		return id;
+	}
+
+	// remove resource from list
+	void erase(resource_id id)
+	{
+		error_string = "No error.";
+		if (!stored_list.empty())
+		{
+			deleted_list.push(id);
+			stored_list.erase(id);
+		}
+		else
+		{
+			error_string = "Trying to remove resource from empty list!";
+			return;
+		}
+	}
+
+	// get resource from list
+	_Ty get(resource_id id)
+	{
+		error_string = "No error.";
+		if (!exists(id))
+		{
+			error_string = "Trying to accessing empty list!";
+			return _Ty(0);
+		}
+		return stored_list.at(id);
+	}
+
+	// check resource exists in id
+	bool exists(resource_id id)
+	{
+		error_string = "No error.";
+		return ((!empty()) && (stored_list.count(id) > 0));
+	}
+
+	// check if list is empty
+	bool empty()
+	{
+		error_string = "No error.";
+		return stored_list.empty();
+	}
+
+	// check list size
+	std::size_t size()
+	{
+		error_string = "No error.";
+		return stored_list.size();
+	}
+
+	// get list begin
+	typename std::unordered_map<resource_id, _Ty>::iterator begin() noexcept
+	{
+		return stored_list.begin();
+	}
+
+	typename std::unordered_map<resource_id, _Ty>::const_iterator cbegin() const noexcept
+	{
+		return stored_list.cbegin();
+	}
+
+	// get list end
+	typename std::unordered_map<resource_id, _Ty>::iterator end() noexcept
+	{
+		return stored_list.end();
+	}
+
+	typename std::unordered_map<resource_id, _Ty>::const_iterator cend() const noexcept
+	{
+		return stored_list.cend();
+	}
+
+	// operator override
+	_Ty operator[](resource_id key) const
+	{
+		error_string = "No error.";
+		return stored_list[key];
+	}
+
+	// get error string
+	std::string get_error_string()
+	{
+		return error_string;
+	}
+
+	// get list
+	std::unordered_map<resource_id, _Ty> list()
+	{
+		error_string = "No error.";
+		return stored_list;
+	}
+
+private:
+	// error string
+	std::string error_string;
+	// containers
+	std::unordered_map<resource_id, _Ty> stored_list;
+	std::priority_queue<resource_id, std::vector<resource_id>, std::greater<resource_id>> deleted_list;
+
+};
+
+#endif
